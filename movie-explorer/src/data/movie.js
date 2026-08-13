ba4e76f6c646efefc53b55ccc9f4ffe582a3878d
Urvashi Zinzuvadiya <urvashi7211@gmail.com>
2026-08-13 22:38:14 +0530

    image path change

--- movie-explorer/src/data/movie.js
+++ movie-explorer/src/data/movie.js
@@ -2,22 +2,22 @@ export const popularMovies = [
     {
         id: 1,
         title: "Barbie (2023)",
-        image: "/image/poster/Barbie (2023).jpg"
+        image: "/public/poster/Barbie (2023).jpg"
     },
     {
         id: 2,
         title: "Oppenheimer (2023)",
-        image: "/image/poster/Oppenheimer (2023).jpg"
+        image: "/public/poster/Oppenheimer (2023).jpg"
     },
     {
         id: 3,
         title: "Dune 2",
-        image: "/image/poster/dune2.jpg"
+        image: "/public/poster/dune2.jpg"
     },
     {
         id: 4,
         title: "Inside Out 2 (2024)",
-        image: "/image/poster/Inside Out 2 (2024).jpg"
+        image: "/public/poster/Inside Out 2 (2024).jpg"
     }
 ];
 
@@ -26,22 +26,22 @@ export const topRated = [
     {
         id: 5,
         title: "The Shawshank Redemption",
-        image: "/image/poster/The Shawshank Redemption.jpg"
+        image: "/public/poster/The Shawshank Redemption.jpg"
     },
     {
         id: 6,
         title: "The Godfather",
-        image: "/image/poster/The Godfather.jpg"
+        image: "/public/poster/The Godfather.jpg"
     },
     {
         id: 7,
         title: "The Dark Knight",
-        image: "/image/poster/The Dark Knight.jpg"
+        image: "/public/poster/The Dark Knight.jpg"
     },
     {
         id: 8,
         title: "12 Angry Men",
-        image: "/image/poster/12 Angry Men.jpg"
+        image: "/public/poster/12 Angry Men.jpg"
     }
 ];
 
@@ -50,22 +50,22 @@ export const nowPlaying = [
     {
         id: 9,
         title: "Superman (2025)",
-        image: "/image/poster/Superman (2025).jpg"
+        image: "/public/poster/Superman (2025).jpg"
     },
     {
         id: 10,
         title: "The Fantastic Four - First Steps",
-        image: "/image/poster/The Fantastic Four-First Steps.jpg"
+        image: "/public/poster/The Fantastic Four-First Steps.jpg"
     },
     {
         id: 11,
         title: "Jurassic World Rebirth",
-        image: "/image/poster/Jurassic World Rebirth.jpg"
+        image: "/public/poster/Jurassic World Rebirth.jpg"
     },
     {
         id: 12,
         title: "The Naked Gun (2025)",
-        image: "/image/poster/The Naked Gun (2025).jpg"
+        image: "/public/poster/The Naked Gun (2025).jpg"
     }
 ];
 
@@ -74,22 +74,22 @@ export const upcoming = [
     {
         id: 13,
         title: "Avatar - Fire and Ash",
-        image: "/image/poster/Avatar- Fire and Ash.jpg"
+        image: "/public/poster/Avatar- Fire and Ash.jpg"
     },
     {
         id: 14,
         title: "Spider-Man - Brand New Day",
-        image: "/image/poster/Spider-Man - Brand New Day.jpg"
+        image: "/public/poster/Spider-Man - Brand New Day.jpg"
     },
     {
         id: 15,
         title: "Shrek 5",
-        image: "/image/poster/Shrek 5.jpg"
+        image: "/public/poster/Shrek 5.jpg"
     },
     {
         id: 16,
         title: "The Super Mario Bros. Movie 2",
-        image: "/image/poster/The Super Mario Bros ,Movie 2.jpg"
+        image: "/public/poster/The Super Mario Bros ,Movie 2.jpg"
     }
 ];
 
@@ -98,22 +98,22 @@ export const actionMovies = [
     {
         id: 17,
         title: "John Wick - Chapter 4",
-        image: "/image/poster/John Wick - Chapter 4.jpg"
+        image: "/public/poster/John Wick - Chapter 4.jpg"
     },
     {
         id: 18,
         title: "Mad Max - Fury Road",
-        image: "/image/poster/Mad Max - Fury Road.jpg"
+        image: "/public/poster/Mad Max - Fury Road.jpg"
     },
     {
         id: 19,
         title: "Mission Impossible - Dead Reckoning",
-        image: "/image/poster/Mission -Impossible – Dead Reckoning.jpg"
+        image: "/public/poster/Mission -Impossible – Dead Reckoning.jpg"
     },
     {
         id: 20,
         title: "Extraction 2",
-        image: "/image/poster/Extraction 2.jpg"
+        image: "/public/poster/Extraction 2.jpg"
     }
 ];
 
@@ -122,22 +122,22 @@ export const comedyMovies = [
     {
         id: 21,
         title: "Free Guy",
-        image: "/image/poster/Free Guy.jpg"
+        image: "/public/poster/Free Guy.jpg"
     },
     {
         id: 22,
         title: "The Hangover",
-        image: "/image/poster/The Hangover.jpg"
+        image: "/public/poster/The Hangover.jpg"
     },
     {
         id: 23,
         title: "Deadpool",
-        image: "/image/poster/Deadpool.jpg"
+        image: "/public/poster/Deadpool.jpg"
     },
     {
         id: 24,
         title: "Jumanji - Welcome to the Jungle",
-        image: "/image/poster/Jumanji - Welcome to the Jungle.jpg"
+        image: "/public/poster/Jumanji - Welcome to the Jungle.jpg"
     }
 ];
 
@@ -146,22 +146,22 @@ export const romanceMovies = [
     {
         id: 25,
         title: "Titanic",
-        image: "/image/poster/Titanic.jpg"
+        image: "/public/poster/Titanic.jpg"
     },
     {
         id: 26,
         title: "La La Land",
-        image: "/image/poster/La La Land.jpg"
+        image: "/public/poster/La La Land.jpg"
     },
     {
         id: 27,
         title: "The Notebook",
-        image: "/image/poster/The Notebook.jpg"
+        image: "/public/poster/The Notebook.jpg"
     },
     {
         id: 28,
         title: "Me Before You",
-        image: "/image/poster/Me Before You.jpg"
+        image: "/public/poster/Me Before You.jpg"
     }
 ];
 
@@ -170,21 +170,21 @@ export const horrorMovies = [
     {
         id: 29,
         title: "The Conjuring",
-        image: "/image/poster/The Conjuring.jpg"
+        image: "/public/poster/The Conjuring.jpg"
     },
     {
         id: 30,
         title: "IT",
-        image: "/image/poster/IT.jpg"
+        image: "/public/poster/IT.jpg"
     },
     {
         id: 31,
         title: "Smile 2",
-        image: "/image/poster/Smile 2.jpg"
+        image: "/public/poster/Smile 2.jpg"
     },
     {
         id: 32,
         title: "A Quiet Place",
-        image: "/image/poster/A Quiet Place.jpg"
+        image: "/public/poster/A Quiet Place.jpg"
     }
 ];
